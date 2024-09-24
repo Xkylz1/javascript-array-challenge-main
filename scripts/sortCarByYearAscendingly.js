37387c3c33b0be30bb33abdfece82ccbfa068dd2
xkylz1 <fauzansaputra741@gmail.com>
2024-09-24 18:10:16 +0800

    fix: ascending sort with buble sort

--- scripts/sortCarByYearAscendingly.js
+++ scripts/sortCarByYearAscendingly.js
@@ -8,6 +8,29 @@ function sortCarByYearAscendingly(cars) {
 
   // Tulis code-mu disini
 
+      let i, j, temp;
+      let swapped;
+      for (i = 0; i < result.length - 1; i++) 
+      {
+          swapped = false;
+          for (j = 0; j < result.length - i - 1; j++) 
+          {
+              if (result[j].year > result[j + 1].year) 
+              {
+                  // Swap arr[j] and arr[j+1]
+                  temp = result[j];
+                  result[j] = result[j + 1];
+                  result[j + 1] = temp;
+                  swapped = true;
+              }
+          }
+  
+          // IF no two elements were 
+          // swapped by inner loop, then break
+          if (swapped == false)
+          break;
+      }
+  
   // Rubah code ini dengan array hasil sorting secara ascending
   return result;
 }
